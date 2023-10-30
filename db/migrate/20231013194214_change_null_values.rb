5048a03409fd5d453a14a6d26fa918441c154328
dafeys <dafeys@outlook.com>
2023-10-31 01:39:22 +0200

    fix: edit migration

--- db/migrate/20231013194214_change_null_values.rb
+++
@@ -1,7 +0,0 @@
-class ChangeNullValues < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :books, :title, false
-    change_column_null :books, :author, false
-    change_column_null :books, :isbn, false
-  end
-end
