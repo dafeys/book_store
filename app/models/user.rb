c40898ddbd26a5737e49c8608cebbfcaa227d8ba
dafeys <dafeys@outlook.com>
2023-11-02 23:47:37 +0200

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
+
+  validates :username, :first_name, :last_name, :email, presence: true
+  validates :email, :username, uniqueness: true
+  validates :username, length: { minimum: 3, maximum: 15 },
+                       format: { with: /\A[a-z0-9_]+\z/,
+                       message: "only allows lowercase letters, numbers, and underscores" }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
