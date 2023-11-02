5c48304a10a066018de46b2dab370c890e26a5a0
dafeys <dafeys@outlook.com>
2023-11-02 23:49:55 +0200

    strict routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "books#index"
   resources :books
-  devise_for :users
+  devise_for :users, skip: [:password, :registrations]
+
+  as :user do
+    get "users/sign_up", to: "devise/registrations#new", as: :new_user_registration
+    post "users", to: "devise/registrations#create", as: :user_registration
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
