30b3b93c18bae1dd7ada08ce63c4414a25ae2d1f
dafeys <dafeys@outlook.com>
2023-11-02 12:01:52 +0200

    fixes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,17 +3,14 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   context "validations" do
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_length_of(:title).is_at_least(1) }
-    it { is_expected.to validate_length_of(:title).is_at_most(50) }
+    it { is_expected.to validate_length_of(:title).is_at_least(1).is_at_most(50)}
+
 
     it { is_expected.to validate_presence_of(:author) }
-    it { is_expected.to validate_length_of(:author).is_at_least(1) }
-    it { is_expected.to validate_length_of(:author).is_at_most(50) }
+    it { is_expected.to validate_length_of(:author).is_at_least(1).is_at_most(50)  }
 
     it { is_expected.to validate_presence_of(:isbn) }
     it { is_expected.to validate_length_of(:isbn).is_equal_to(13) }
-    it { is_expected.to allow_value("1234567890123").for(:isbn) }
-    it { is_expected.not_to allow_value("12345678901234").for(:isbn) }
 
     it { is_expected.to validate_length_of(:description).is_at_most(1000).allow_nil }
   end
