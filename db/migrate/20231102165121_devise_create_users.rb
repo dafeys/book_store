a66dd50e4dbac475caac825dae9939fe72fb525d
dafeys <dafeys@outlook.com>
2023-11-03 02:54:55 +0200

    fix fields in db

--- db/migrate/20231102165121_devise_create_users.rb
+++ db/migrate/20231102165121_devise_create_users.rb
@@ -1,25 +1,16 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      ## Database authenticatable
-      t.string :username,           null: false, default: ""
-      t.string :first_name,         null: false, default: ""
-      t.string :last_name,          null: false, default: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
+      t.string :username, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
       t.datetime :remember_created_at
-
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :username,             unique: true
-    add_index :users, :reset_password_token, unique: true
   end
 end
