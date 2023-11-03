f9df3a50a5b27f1b068889ecd2499043bee0ff1c
dafeys <dafeys@outlook.com>
2023-11-03 23:50:45 +0200

    add styling

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     title { Faker::Book.title }
     author { Faker::Name.name }
     isbn { Faker::Number.decimal_part(digits: 13) }
-    description { Faker::Lorem.paragraph }
+    description { Faker::Lorem.paragraph_by_chars(number: rand(200..500), supplemental: true) }
 
     trait :without_title do
       title { "" }
