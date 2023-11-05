c812655368ece40b7e2e5f0a189630d4f511dea1
Ivan Fedai <36037839+dafeys@users.noreply.github.com>
2023-11-05 16:35:16 +0200

    Update spec/models/book_spec.rb
    Co-authored-by: Ivan Marynych <49816584+loqimean@users.noreply.github.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Book, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_length_of(:title).is_at_least(1).is_at_most(50)}
 
-
     it { is_expected.to validate_presence_of(:author) }
     it { is_expected.to validate_length_of(:author).is_at_least(1).is_at_most(50)  }
 
