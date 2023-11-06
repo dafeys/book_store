99f0eb90a049c7ab8bd62c95156ef625215b92d7
dafeys <dafeys@outlook.com>
2023-11-06 04:03:57 +0200

    add atachments to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  has_one_attached :book_cover
+  has_one_attached :book_content
+
   validates :title, :author, presence: true, length: { in: 1..50 }
   validates :isbn, presence: true, length: { is: 13 }
   validates :description, length: { maximum: 1000 }
