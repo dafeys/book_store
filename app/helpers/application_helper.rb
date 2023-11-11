0b75a1d6e8649e2ed50a1e07121b5ec0832c5643
dafeys <dafeys@outlook.com>
2023-11-11 16:20:24 +0200

    refactoring of the cover display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_book_cover(book)
+    if book.book_cover.attached?
+      image_tag book.book_cover, class: "book-img"
+    else
+      image_tag "book_cover.jpg", class: "book-img"
+    end
+  end
 end
