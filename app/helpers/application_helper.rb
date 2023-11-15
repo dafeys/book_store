94a087357f21e6d45f1bbb85602324ee6b2cd723
Ivan Fedai <36037839+dafeys@users.noreply.github.com>
2023-11-15 19:39:16 +0200

    Add empty line app/helpers/application_helper.rb
    Co-authored-by: Ivan Marynych <49816584+loqimean@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def display_book_cover(book)
     image = book.book_cover.attached? ? book.book_cover : "book_cover.jpg"
+
     image_tag image, class: "book-img"
   end
 end
