0b78e5835bf8c53c49ec57fbd6fb960fc713ca5d
dafeys <dafeys@outlook.com>
2023-11-21 03:32:11 +0200

    fix books_index

--- app/chewy/books_index.rb
+++ app/chewy/books_index.rb
@@ -1,5 +1,7 @@
 class BooksIndex < Chewy::Index
+  index_name { "books_#{Rails.env}" }
   index_scope Book
-  field :title
-  field :author
+
+  field :title, analyzer: "standard"
+  field :author, analyzer: "standard"
 end
